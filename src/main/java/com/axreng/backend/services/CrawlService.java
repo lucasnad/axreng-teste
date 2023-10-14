f2c8f342fd72dea6d7cd3d98051dff69eb29e7e4
lucasnad <44577210+lucasnad@users.noreply.github.com>
2023-10-14 18:17:28 -0300

    fix: ajustando para alterar a forma de busca

--- src/main/java/com/axreng/backend/services/CrawlService.java
+++ src/main/java/com/axreng/backend/services/CrawlService.java
@@ -8,7 +8,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,24 +19,50 @@ public class CrawlService {
     private static final String LINK_REGEX = "href=[\"']([^\"']+)[\"']";
     private static final Pattern LINK_PATTERN = Pattern.compile(LINK_REGEX, Pattern.CASE_INSENSITIVE);
 
+    private Set<String> visitedUrls = new HashSet<>();
+
     public List<String> crawlForTerm(String keyword) {
         String baseUrl = getBaseUrl();
-        String content = fetchContent(baseUrl);
-        return extractLinks(content, baseUrl, keyword.toLowerCase());
+        List<String> foundUrls = new ArrayList<>();
+        searchDFS(baseUrl, keyword, foundUrls);
+        return foundUrls;
+    }
+
+    private void searchDFS(String currentUrl, String keyword, List<String> foundUrls) {
+        if (visitedUrls.contains(currentUrl)) {
+            return;
+        }
+        visitedUrls.add(currentUrl);
+
+        String content = fetchContent(currentUrl);
+        if (content.toLowerCase().contains(keyword.toLowerCase())) {
+            foundUrls.add(currentUrl);
+        }
+
+        List<String> links = extractLinks(content, currentUrl);
+        for (String link : links) {
+            searchDFS(link, keyword, foundUrls);
+        }
     }
 
     private String getBaseUrl() {
         String baseUrl = System.getenv("BASE_URL");
+
+        if(baseUrl == null){
+            baseUrl = "http://hiring.axreng.com/";
+        }
+
         if (baseUrl == null || baseUrl.isEmpty()) {
             throw new IllegalStateException("BASE_URL environment variable is not set");
         }
+
         return baseUrl.endsWith("/") ? baseUrl.substring(0, baseUrl.length() - 1) : baseUrl;
     }
 
-    private String fetchContent(String baseUrl) {
+    private String fetchContent(String url) {
         try {
-            URL url = new URL(baseUrl);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            URL targetUrl = new URL(url);
+            HttpURLConnection connection = (HttpURLConnection) targetUrl.openConnection();
             try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                 StringBuilder content = new StringBuilder();
                 String line;
@@ -44,24 +72,29 @@ public class CrawlService {
                 return content.toString();
             }
         } catch (Exception e) {
-            logger.error(e.getMessage(), e);
-            throw new RuntimeException("Failed to fetch content from: " + baseUrl, e);
+            logger.error("Failed to fetch content from: " + url, e);
+            return ""; // retornando conteúdo vazio em caso de erro
         }
     }
 
-    private List<String> extractLinks(String content, String baseUrl, String keyword) {
-        List<String> foundUrls = new ArrayList<>();
+    /**
+     *
+     * @param content
+     * @param baseUrl
+     * @return
+     */
+    private List<String> extractLinks(String content, String baseUrl) {
+        List<String> links = new ArrayList<>();
         Matcher matcher = LINK_PATTERN.matcher(content);
         while (matcher.find()) {
             String link = matcher.group(1);
             if (!link.startsWith("http") && !link.startsWith(baseUrl)) {
                 link = baseUrl + (link.startsWith("/") ? "" : "/") + link;
             }
-            if (link.startsWith(baseUrl) && content.contains(keyword)) {
-                foundUrls.add(link);
+            if (link.startsWith(baseUrl)) {
+                links.add(link);
             }
         }
-        return foundUrls;
+        return links;
     }
-
 }
