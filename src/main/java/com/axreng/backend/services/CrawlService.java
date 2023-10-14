d43207b8f021648a9e51dccc19525fc0798b1581
lucasnad <44577210+lucasnad@users.noreply.github.com>
2023-10-14 17:29:13 -0300

    fix: atualizando busca para pegar links relativos e absolutos

--- src/main/java/com/axreng/backend/services/CrawlService.java
+++ src/main/java/com/axreng/backend/services/CrawlService.java
@@ -9,6 +9,8 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CrawlService {
     private static final Logger logger = LoggerFactory.getLogger(CrawlService.class);
@@ -17,25 +19,51 @@ public class CrawlService {
         List<String> foundUrls = new ArrayList<>();
         String baseUrl = System.getenv("BASE_URL");
 
+        //TODO remover esse bloco antes de enviar;
+        if(baseUrl == null){
+            baseUrl = "http://hiring.axreng.com/";
+        }
+
+        if(baseUrl == null || baseUrl.isEmpty()){
+            throw new IllegalStateException("BASE_URL environment variable is not set");
+        }
+
+        keyword = keyword.toLowerCase(); // Convertendo a palavra-chave para lowercase
+
         try {
             URL url = new URL(baseUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String inputLine;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String line;
             StringBuilder content = new StringBuilder();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
+            while ((line = reader.readLine()) != null) {
+                content.append(line);
             }
-            in.close();
+            reader.close();
 
-            if (content.toString().toLowerCase().contains(keyword.toLowerCase())) {
+            // Verificando se o conteúdo contém a palavra-chave (case insensitive)
+            if (content.toString().toLowerCase().contains(keyword)) {
                 foundUrls.add(baseUrl);
             }
+
+            // Usando uma expressão regular simples para extrair links
+            Pattern pattern = Pattern.compile("href=\"(.*?)\"");
+            Matcher matcher = pattern.matcher(content.toString());
+            while (matcher.find()) {
+                String link = matcher.group(1);
+                if (!link.startsWith("http")) {
+                    link = baseUrl + link; // Convertendo link relativo para absoluto
+                }
+                if (link.startsWith(baseUrl)) {
+                    foundUrls.add(link);
+                }
+            }
         } catch (Exception e) {
-            logger.error(e.getMessage(), e);
+            e.printStackTrace();
         }
 
         return foundUrls;
     }
+
+
 }
