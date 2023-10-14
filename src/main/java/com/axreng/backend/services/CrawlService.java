a60050663750c2a2f1386e81d1854a8e6ebf34f1
lucasnad <44577210+lucasnad@users.noreply.github.com>
2023-10-14 17:36:46 -0300

    fix: ajustando código em módulos

--- src/main/java/com/axreng/backend/services/CrawlService.java
+++ src/main/java/com/axreng/backend/services/CrawlService.java
@@ -14,56 +14,54 @@ import java.util.regex.Pattern;
 
 public class CrawlService {
     private static final Logger logger = LoggerFactory.getLogger(CrawlService.class);
+    private static final String LINK_REGEX = "href=[\"']([^\"']+)[\"']";
+    private static final Pattern LINK_PATTERN = Pattern.compile(LINK_REGEX, Pattern.CASE_INSENSITIVE);
 
     public List<String> crawlForTerm(String keyword) {
-        List<String> foundUrls = new ArrayList<>();
+        String baseUrl = getBaseUrl();
+        String content = fetchContent(baseUrl);
+        return extractLinks(content, baseUrl, keyword.toLowerCase());
+    }
+
+    private String getBaseUrl() {
         String baseUrl = System.getenv("BASE_URL");
-
-        if(baseUrl == null){
-            baseUrl = "http://hiring.axreng.com/";
-        }
-
-        if(baseUrl == null || baseUrl.isEmpty()){
+        if (baseUrl == null || baseUrl.isEmpty()) {
             throw new IllegalStateException("BASE_URL environment variable is not set");
         }
+        return baseUrl.endsWith("/") ? baseUrl.substring(0, baseUrl.length() - 1) : baseUrl;
+    }
 
-        keyword = keyword.toLowerCase(); // Convertendo a palavra-chave para lowercase
-
+    private String fetchContent(String baseUrl) {
         try {
             URL url = new URL(baseUrl);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            String line;
-            StringBuilder content = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                content.append(line);
-            }
-            reader.close();
-
-            // Verificando se o conteúdo contém a palavra-chave (case insensitive)
-            if (content.toString().toLowerCase().contains(keyword)) {
-                foundUrls.add(baseUrl);
-            }
-
-            // Melhorando a expressão regular para extrair links
-            Pattern pattern = Pattern.compile("href=[\"']([^\"']+)[\"']", Pattern.CASE_INSENSITIVE);
-            Matcher matcher = pattern.matcher(content.toString());
-            while (matcher.find()) {
-                String link = matcher.group(1);
-                if (!link.startsWith("http") && !link.startsWith(baseUrl)) {
-                    link = baseUrl + (link.startsWith("/") ? "" : "/") + link; // Convertendo link relativo para absoluto
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                StringBuilder content = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    content.append(line);
                 }
-                if (link.startsWith(baseUrl)) {
-                    foundUrls.add(link);
-                }
+                return content.toString();
             }
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
+            throw new RuntimeException("Failed to fetch content from: " + baseUrl, e);
         }
-
-        return foundUrls;
     }
 
-
+    private List<String> extractLinks(String content, String baseUrl, String keyword) {
+        List<String> foundUrls = new ArrayList<>();
+        Matcher matcher = LINK_PATTERN.matcher(content);
+        while (matcher.find()) {
+            String link = matcher.group(1);
+            if (!link.startsWith("http") && !link.startsWith(baseUrl)) {
+                link = baseUrl + (link.startsWith("/") ? "" : "/") + link;
+            }
+            if (link.startsWith(baseUrl) && content.contains(keyword)) {
+                foundUrls.add(link);
+            }
+        }
+        return foundUrls;
+    }
 
 }
